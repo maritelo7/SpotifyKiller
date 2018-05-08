d4a5280bd52c7989af247d78e29e32de07380017
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-05-08 11:38:59 -0500

    MenuDrawer
    Agregación de librerías al proyecto

--- YouTunes/src/vista/YouTunes.java
+++ YouTunes/src/vista/YouTunes.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package youtunes;
+package vista;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
