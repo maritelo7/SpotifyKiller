8daf28ff8a0276de4fa95fc91be2acd7d8dc7af1
Maribel Tello <zS15011664@estudiantes.uv.mx>
2018-06-17 15:46:07 -0500

    Pruebas json

--- YouTunes/src/vista/controlador/SubirCancionesController.java
+++ YouTunes/src/vista/controlador/SubirCancionesController.java
@@ -8,11 +8,15 @@ package vista.controlador;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,6 +31,7 @@ import javafx.stage.FileChooser;
 import modelo.pojos.Cancion;
 import modelo.pojos.Genero;
 import modelo.pojos.Usuario;
+import org.json.JSONObject;
 
 /**
  * FXML Controller class
@@ -55,8 +60,8 @@ public class SubirCancionesController implements Initializable {
     private Label labelGenero;
     @FXML
     private Label labelCalidad;
-    
-    ObservableList<Cancion> items =FXCollections.observableArrayList(); 
+
+    ObservableList<Cancion> items = FXCollections.observableArrayList();
     Usuario usuario;
 
     /**
@@ -77,10 +82,10 @@ public class SubirCancionesController implements Initializable {
             labelCalidad.setVisible(false);
             comboGenero.setVisible(false);
             comboCalidad.setVisible(false);
-            
+
         }
     }
- 
+
     @FXML
     public void seleccionarCancion() {
         final FileChooser archivoAudio = new FileChooser();
@@ -91,9 +96,8 @@ public class SubirCancionesController implements Initializable {
             try {
                 String path = audio.getAbsolutePath();
                 File theFile = new File(path);
-                System.out.println(theFile.getName());
                 input = new FileInputStream(theFile);
-                //se enviaría el inputStream
+
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(SubirCancionesController.class.getName()).log(Level.SEVERE, null, ex);
             } finally {
@@ -106,59 +110,93 @@ public class SubirCancionesController implements Initializable {
         }
     }
 
-     private static void configureFileChooser(final FileChooser archivoAudio) {
+    private static void configureFileChooser(final FileChooser archivoAudio) {
         archivoAudio.setTitle("Buscar archivo de audio");
         archivoAudio.getExtensionFilters().addAll(
             new FileChooser.ExtensionFilter("MP3", "*.mp3")
         );
     }
-     
-     @FXML
-       private void agregarCancion() {
-           if (validarCampos()){
-               Cancion cancion = new Cancion();
-               cancion.setTitulo(fieldTitulo.getText().substring(0, 99));
-               cancion.setColaboradores(fieldColaboradores.getText().substring(0, 254));
-               cancion.setIdGenero(comboGenero.getValue().getIdGenero());
-               //cancion.setCalidad();
-               //cancion.setIdAlbum();
-               //cancion.setIdUsuarioSubioCancion();
-               //cancion.setFormato();
-               items.add(cancion);
-               listCanciones.setItems(items);
-           }else{
-               //mensaje de llenar al menos el título
-               System.out.println("LLENAR TITULO");
-           }
-       }
-       
-        @FXML
-       private void subirCancion() {
-           if (!items.isEmpty()){
-               //se envía lista de canciones dentro de un ciclo, se envia canción x canción
-               System.out.println("SUBIENDO");
-           }else{
-               //no se puede enviar lista porque es vacía
-               System.out.println("LISTA VACIA");
-           }
-       }
-       
-       
-       private boolean validarCampos(){
-            if (fieldTitulo.getText().trim().length() != 0)
-                return true;       
-            else
-                return false;
-       }
+
+    @FXML
+    private void agregarCancion() {
+        if (validarCampos()) {
+            Cancion cancion = new Cancion();
+            cancion.setTitulo(fieldTitulo.getText().substring(0, 99));
+            cancion.setColaboradores(fieldColaboradores.getText().substring(0, 254));
+            cancion.setIdGenero(comboGenero.getValue().getIdGenero());
+            //cancion.setCalidad();
+            //cancion.setIdAlbum();
+            //cancion.setIdUsuarioSubioCancion();
+            //cancion.setFormato();
+            items.add(cancion);
+            listCanciones.setItems(items);
+        } else {
+            //mensaje de llenar al menos el título
+            System.out.println("LLENAR TITULO");
+        }
+    }
+
+    @FXML
+    private void subirCancion() {
+        System.out.println("SUBIR CANCION");
+        try {
+            //           Cancion cancion = new Cancion();
+//           cancion.setCalidad(1);
+//           cancion.setColaboradores("Sean Kingston");
+//           cancion.setFormato(".mp3");
+//           cancion.setIdAlbum(1);
+//           cancion.setIdGenero(1);
+//           cancion.setIdUsuarioSubioCancion(1);
+//           cancion.setPath("PATH QUE NO SIRVE PA NADA");
+//           cancion.setTitulo("Eenie Meenie");
+            JSONObject cancion = new JSONObject();
+            cancion.accumulate("titulo", "Eenie Meednie");
+            cancion.accumulate("formato", ".mp3");
+            cancion.accumulate("idGenero", 1);
+            cancion.accumulate("path", "C:\\Users\\VictorHugo\\Desktop\\conferencia.jpg");
+            cancion.accumulate("colaboradores", "Sean Kingston");
+            cancion.accumulate("idUsuarioSubioCancion", 1);
+            cancion.accumulate("idAlbum", 1);
+            cancion.accumulate("calidad", 1);
+
+            URL url = new URL("http://192.168.1.82:8000/guardarCancion/"+cancion);
+            //URL url = new URL("http://192.168.1.82:8000/test");
+            URLConnection con = url.openConnection();
+            
+            
+             BufferedReader in = new BufferedReader(new InputStreamReader(
+                con.getInputStream()));
+             String linea;
+            while ((linea = in.readLine()) != null) {
+                System.out.println(linea);
+            }
+            
+            System.out.println("JSON ENVIADO");
+//           if (!items.isEmpty()){
+//
+//               
+//               System.out.println("SUBIENDO");
+//           }else{
+//               //no se puede enviar lista porque es vacía
+//               System.out.println("LISTA VACIA");
+//           }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(SubirCancionesController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SubirCancionesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private boolean validarCampos() {
+        if (fieldTitulo.getText().trim().length() != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-        
-        
-        
-    }
-     
-     
 
-
+}
