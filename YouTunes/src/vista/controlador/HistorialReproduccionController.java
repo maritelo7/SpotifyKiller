2ea204fee641b915f8e9381e3802a5ad3680f743
Maribel Tello <zS15011664@estudiantes.uv.mx>
2018-06-18 01:26:13 -0500

    Historial

--- YouTunes/src/vista/controlador/HistorialReproduccionController.java
+++ YouTunes/src/vista/controlador/HistorialReproduccionController.java
@@ -6,10 +6,19 @@
 package vista.controlador;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import modelo.pojos.Cancion;
+import modelo.pojos.CancionHistorial;
+import modelo.pojos.Historial;
 
 /**
  * FXML Controller class
@@ -19,14 +28,47 @@ import javafx.scene.control.TableView;
 public class HistorialReproduccionController implements Initializable {
 
     @FXML
-    private TableView<?> tableHistorial;
+    private TableView<CancionHistorial> tableHistorial;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        cargarTabla();
+        
     }    
+    
+     public void cargarTabla(){         
+      TableColumn titulo = new TableColumn("Título de la canción");
+      titulo.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+      titulo.setPrefWidth(300);
+      TableColumn fecha = new TableColumn("Fecha de reproducción");
+      fecha.setCellValueFactory(new PropertyValueFactory<>("fecha"));
+      fecha.setPrefWidth(200);
+      tableHistorial.getColumns().addAll(titulo, fecha);
+      tableHistorial.setEditable(false);
+    }
+     
+      public void cargarInformacionTablaConversaciones(){
+      //se recibiría un Historial con canciones
+      Cancion cancion = null;
+      CancionHistorial canHis;
+      List<Historial> historial = null;
+      List<CancionHistorial> cancionHistorial = new ArrayList();
+        for (int i = 0; i < historial.size(); i++) {
+            canHis = new CancionHistorial();
+            canHis.setTitulo(cancion.getTitulo());
+            canHis.setFecha(historial.get(i).getFecha());
+        }
+      
+         ObservableList<CancionHistorial> listaHistorialCanciones = FXCollections.observableArrayList();
+         for (int i = 0; i < cancionHistorial.size(); i++) {
+            listaHistorialCanciones.add(cancionHistorial.get(i));
+         }        
+         tableHistorial.setItems((ObservableList<CancionHistorial>)listaHistorialCanciones);
+      }
+    
+   
     
 }
